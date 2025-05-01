876f0ac364ed1e448f7cdfc03884b4794f014c91
hrmrakib <hrmrakibs@gmail.com>
2025-05-01 14:34:16 +0600

    start a mission to become next level developer

---
+++ typescript-technocrat/Object_Optional_Literal_Type.ts
@@ -0,0 +1,15 @@
+const user  : {
+    company: "Join Verture AI",  // type literal
+    firstName: string,
+    middleName?: string,
+    lastName: string,
+    isMarried: boolean
+} = {
+ company: "Join Verture AI",
+    firstName: "Md",
+    middleName: "Rakibul",
+    lastName: "Alam",
+    isMarried: false
+}
+
+console.log(user)
---
+++ typescript-technocrat/dist/Object_Optional_Literal_Type.js
@@ -0,0 +1,9 @@
+"use strict";
+const user = {
+    company: "Join Verture AI",
+    firstName: "Md",
+    middleName: "Rakibul",
+    lastName: "Alam",
+    isMarried: false
+};
+console.log(user);
