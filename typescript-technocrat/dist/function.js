fa1e8242ed3b95253d04738fa7ba6ce91ec69f16
hrmrakib <hrmrakibs@gmail.com>
2025-05-02 11:10:35 +0600

    Implement sumAll function to calculate the sum of an array of numbers

--- typescript-technocrat/dist/function.js
+++ typescript-technocrat/dist/function.js
@@ -6,3 +6,9 @@ function sum(a, b, c = 0, d) {
 const add1 = sum(29, 47);
 console.log(add1);
 // rest
+function sumAll(...numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
+}
+const values = [10, 20, 33, 49, 41, 78, 96];
+const x = sumAll(...values);
+console.log(x);
--- typescript-technocrat/function.ts
+++ typescript-technocrat/function.ts
@@ -8,3 +8,10 @@ const add1 = sum(29, 47);
 console.log(add1)
 
 // rest
+function sumAll (...numbers: number[]) {
+    return numbers.reduce((acc, num)=> acc + num, 0);
+}
+
+const values = [10,20, 33, 49, 41, 78, 96]
+const x = sumAll(...values)
+console.log(x)
